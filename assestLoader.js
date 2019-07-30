1cb8f12d73b5bc698bdee722ff3d2566c288f77f
K.Y <K.Y.@not_valid.co.uk>
2019-07-30 16:44:43 +0200

    Basic script functionality implemented

--- assestLoader.js
+++ assestLoader.js
@@ -1,21 +1,55 @@
 function loadfile() {
-    var scriptQeue = [
+    let scriptQeue = [];
 
-    ];
-    function script(scriptPath, optionsOrCallback) {
+    function buildTag(scriptObject){
+        return new Promise((resolve, reject) => {
+                try{
+                    let script = document.createElement('script');
+                    script.src = scriptObject.path;
+                    scriptObject.id ? script.id = scriptObject.id : null;
+                    scriptObject.class ? script.classList.add(scriptObject.class) : null;
+                    scriptObject.async ? script.async = "async" : null;
+                    scriptObject.defer ? script.defer = "defer" : null;
+                    script.onload = function(){
+                        typeof scriptObject.callback === 'function' ? scriptObject.callback() : null;
+                    };
+                    script.onerror = function(){
+                        this.parentNode.removeChild(this);
+                    }
+                    document.head.appendChild(script) === script ? resolve(true) : reject(false);
+                }
+                catch(err){
+                    reject(err)
+                }
+            }
+        ).catch(e => {
+            return e
+        })
+    }
+    function script(scriptPath, optionsOrCallback = {}) {
+        let def = {
+            path: scriptPath,
+            id: optionsOrCallback.id || null,
+            class: optionsOrCallback.class || null,
+            inline: optionsOrCallback.inline || false,
+            async: optionsOrCallback.async || false,
+            defer: optionsOrCallback.defer || false,
+            callback: optionsOrCallback.callback || null
+        };
         if (typeof optionsOrCallback === 'function') {
-            
+            def.callback = optionsOrCallback;
         }
-        else if (typeof optionsOrCallback === 'object') {
-            
-        }
-        else {
-
-        }
+        scriptQeue.push(buildTag(def));
         return this;
     };
     function wait() {
-        console.log('waiting');
+        Promise
+            .all(scriptQeue)
+            .then(
+                scripts => {
+                    console.log(scripts)
+                }
+            )
         return this;
     };
     function done() {
@@ -28,11 +62,3 @@ function loadfile() {
         'done' : done
     })
 }
-
-loadfile().script("/script/bundle/minify/?event@2.0.0&ui@1.0.0", {
-    id: 'plop',
-    class: 'dynamicScript',
-    async: true,
-    defer: true,
-    callback: console.log
-})
